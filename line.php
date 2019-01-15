c637484182f44aa4784615e15a38220a5dbf0232
jakrapat1998 <46474305+jakrapat1998@users.noreply.github.com>
2019-01-15 10:32:02 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'rUoVM4v5YeWo5d7F4WttsqyStACMVV4TlmYTymKz5MA',
+        'to' => 'Ue77a191627f6ac91899e75d92264310c',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
